ab3b39eba497e90b638c06601afc52c96707bfc6
AydosNB <nawrizbaevaydos50@gmail.com>
2025-01-26 00:36:55 +0500

    edit profile menu

--- src/components/page-comp/ProfileButton.jsx
+++ src/components/page-comp/ProfileButton.jsx
@@ -1,6 +1,6 @@
 import { CgLogIn } from "react-icons/cg";
 import { BsPersonCircle } from "react-icons/bs";
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { motion } from "motion/react";
 import { useDispatch } from "react-redux";
 import { fetchUserProfile } from "../../store/userSlice/userSlice";
@@ -10,15 +10,32 @@ const ProfileButton = ({ userData }) => {
     const [isActive, setIsActive] = useState(false)
     const dispatch = useDispatch()
 
-    const logoutProfile = () => {
-        localStorage.removeItem("authToken")
-        dispatch(fetchUserProfile())
-        setIsActive(false)
-        succsessToast("You have successfully logged out.")
+    const handleClickOutside = (event) => {
+        if (!event.target.closest(".menu")) {
+            setIsActive(false); // Menyu tashqarisiga bosilganda yopish
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const logoutProfile = useCallback(() => {
+        localStorage.removeItem("authToken");
+        dispatch(fetchUserProfile());
+        setIsActive(false);
+        succsessToast("You have successfully logged out.");
+    }, [dispatch]);
+
+    const truncateText = (text, maxLength) => {
+        return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
     }
     return (
-        <div className="relative">
-            <button onClick={() => setIsActive(prev => !prev)} className="btn btn-primary flex justify-center items-center gap-1">
+        <div className="relative menu">
+            <button onClick={(e) => setIsActive(prev => !prev)} className="btn btn-primary flex justify-center items-center gap-1">
                 <span>
                     <BsPersonCircle />
                 </span>
@@ -33,13 +50,13 @@ const ProfileButton = ({ userData }) => {
                     whileInView={{ opacity: [0, 1], y: 90, scale: 1 }}
                     viewport={{ once: false, amount: 0.1 }}
                     transition={{ duration: 0.2, ease: "easeInOut", delay: 0.1 }}
-                    className="absolute flex flex-col gap-1 bg-white p-[7px] text-gray-600 rounded-sm border-[1px] shadow-md z-[5] min-w-[150px] right-[-12px] bottom-[0] translate-y-[110%]">
+                    className="absolute menu flex flex-col gap-1 bg-white p-[7px] text-gray-600 rounded-sm border-[1px] shadow-md z-[5] min-w-[150px] right-[-12px] bottom-[0] translate-y-[110%]">
                     <button className="text-[14px] font-medium flex p-[5px] rounded-sm hover:bg-slate-100 justify-start items-center gap-1 active:scale-95">
                         <span>
                             <BsPersonCircle />
                         </span>
                         <span className="min-w-max">
-                            {userData.userName.length > 12 ? userData.userName.slice(0, 12) + "..." : userData.userName}
+                            {truncateText(userData.userName, 12)}
                         </span>
                     </button>
                     <hr />
